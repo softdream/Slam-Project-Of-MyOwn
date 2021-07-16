090b3fafb50749a8208e9c9566330ef933a35203
道锋 <1336077399@qq.com>
2021-07-16 09:36:00 +0800

    Update IMUOdomFusionEKF.cpp

--- IMUOdomFusionEKF.cpp
+++ IMUOdomFusionEKF.cpp
@@ -121,6 +121,46 @@ Eigen::Matrix<float, 6, 1> EKF::getStateX()
 	return X;
 }
 
+void EKF::setL( const float l )
+{
+	this->l = l;
+}
+
+void EKF::setDeltaT( const float dt )
+{
+	this->dt = dt;
+}
+
+void EKF::setNoiseR( const Eigen::Matrix<float, 4, 4> &R )
+{
+	this->R = R;
+}
+	
+void EKF::setNoiseR( const float r1, const float r2, const float r3, const float r4 )
+{
+	R(0, 0) = r1;
+	R(1, 1) = r2;
+	R(2, 2) = r3;
+	R(3, 3) = r4;
+}
+	
+void EKF::setNoiseQ( const Eigen::Matrix<float, 6, 6> &Q )
+{
+	this->Q = Q;
+}
+	
+void setNoiseQ( const float q1, const float q2, const float q3, 
+		      	const float q4, const float q5, const float q6 )
+{
+	Q(0, 0) = q1;
+	Q(1, 1) = q2;
+	Q(2, 2) = q3;
+	Q(3, 3) = q4;
+	Q(4, 4) = q5;
+	Q(5, 5) = q6;
+}
+	
+	
 	
 }
 
